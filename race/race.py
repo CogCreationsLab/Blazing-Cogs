a77d73d2777e7118af222eac585b7a359b58f9a6
Cog Creations Lab <59899391+CogCreationsLab@users.noreply.github.com>
2020-09-05 20:56:32 -0400

    Update race.py

--- race/race.py
+++ race/race.py
@@ -88,7 +88,7 @@ class Race(commands.Cog):
         current = await self.config.guild(ctx.guild).Games_Played()
         await self.config.guild(ctx.guild).Games_Played.set(current + 1)
         await ctx.send(
-            f"🚩 A @race has begun! Type **__!b race enter__** "
+            f"🚩 A <@!751696579286663242> has begun! Type **__!b race enter__** "
             f"to join the race! 🚩\nThe race will begin in "
             f"{wait} seconds!\n\n**{ctx.author.mention}** entered the race!"
         )
