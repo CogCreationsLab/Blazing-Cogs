60e8013d589971c32d72b04479b5c27d002eb0e1
Cog Creations Lab <59899391+CogCreationsLab@users.noreply.github.com>
2020-09-08 00:41:37 -0400

    Update race.py

--- race/race.py
+++ race/race.py
@@ -88,9 +88,9 @@ class Race(commands.Cog):
         current = await self.config.guild(ctx.guild).Games_Played()
         await self.config.guild(ctx.guild).Games_Played.set(current + 1)
         await ctx.send(
-            f"🚩 A <@&751696579286663242> has begun! Type **__!b race enter__** "
-            f"to join the race! 🚩\nThe race will begin in "
-            f"{wait} seconds!\n\n**{ctx.author.mention}** entered the race!"
+            f"🚩 A **{ctx.author.mention}** has started a race! Type **__!b race enter__** "
+            f"to join the race! 🚩\nThe <@&751696579286663242> will begin in "
+            f"{wait} seconds!
         )
         await asyncio.sleep(wait)
         self.started = True
