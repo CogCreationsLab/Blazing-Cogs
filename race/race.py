cda4067a027d18a7f0aec0188c52cbd1f4474c9c
Cog Creations Lab <59899391+CogCreationsLab@users.noreply.github.com>
2020-09-09 16:51:22 -0400

    Update race.py

--- race/race.py
+++ race/race.py
@@ -84,9 +84,9 @@ class Race(commands.Cog):
             return await ctx.send("A race is already in progress!  Type `[p]race enter` to enter!")
         self.active = True
         
-            elif not self.bank_check(settings, author):
+        elif not self.bank_check(settings, author):
                 return await self.bot.say("You do not meet the cost of entry. You need atleast {} credits.".format(cost))
-            elif len(data['Players']) == 10:
+        elif len(data['Players']) == 10:
                 return await self.bot.say("There are no more spots left in the race!")
             else:
                 bank = self.bot.get_cog('Economy').bank
